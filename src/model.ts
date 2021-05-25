db613272d3f7f4d198bc92a55d06e55030eb0e61
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-05-25 08:51:12 +0300

    readonly and optional params in interface

--- src/model.ts
+++ src/model.ts
@@ -7,9 +7,11 @@ let originalCost: number = 425;
 interface InventoryItem {
     displayName: string,
     inventoryType: string,
-    trackingNumber: string,
+    readonly trackingNumber: string, //cannot change the value if is set to readonly
     createDate: Date,
-    originalCost: number
+    originalCost?: number //might not always have original cost
+
+    addNote?: (note: string) => string;
 }
 
 function getInventoryItem(trackingNumber: string): InventoryItem {
